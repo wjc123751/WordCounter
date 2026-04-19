3f2fdf654de48f9b391baab2e0ffb7a21d622bd6
wjc123751 <1600905060@qq.com>
2026-04-19 17:56:00 +0800

    修改txt位置

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # WordCounter
+Volume serial number is 3625-0BE1
+C:.
+│   .gitignore
+│   CMakeLists.txt
+│   README.md
+├───build
+│   ├───test.txt     
+│   └───WordCounter.exe        
+├───data
+├───include
+│       wordcounter.h
+└───src
+        main.cpp
+        wordcounter.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,14 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+// 获取 exe 所在的文件夹（核心！）
+fs::path getExeDir()
+{
+    wchar_t buffer[MAX_PATH];
+    GetModuleFileNameW(NULL, buffer, MAX_PATH);
+    return fs::path(buffer).parent_path();
+}
+
 int main()
 {
 #ifdef _WIN32
@@ -18,18 +26,23 @@ int main()
 
     // cout << "当前工作目录: " << fs::current_path() << endl;
 
+    // 重点：获取 exe 所在目录的 test.txt
+    fs::path txtPath = getExeDir() / "test.txt";
+
     WordCounter counter;
 
-    if (counter.countFromFile("../data/test.txt")) {
+    if (counter.countFromFile(txtPath.string())) {
         counter.printResult();
     }
 
-    cout << "\n===== 从外部获取单词列表 =====\n";
-    auto result = counter.getWordMap();
-    for (const auto& p : result)
-    {
-        cout << "单词：" << p.first << " 出现次数：" << p.second << endl;
-    }
+    // cout << "\n===== 从外部获取单词列表 =====\n";
+    // auto result = counter.getWordMap();
+    // for (const auto& p : result)
+    // {
+    //     cout << "单词：" << p.first << " 出现次数：" << p.second << endl;
+    // }
+
+    system("pause");
 
     return 0;
 }
